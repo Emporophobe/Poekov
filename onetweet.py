49e58eacb61c7bd164c3c198732f14c43fb21e8e
Emporophobe <emporophobe@gmail.com>
2014-05-13 11:48:27 -0400

    Added Single Tweets
    For use with cron to schedule tweets instead of using time.sleep

---
+++ onetweet.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+import cPickle as pickle
+import tweepy
+import time
+
+#Tweepy setup
+
+keys = [line.rstrip('\n') for line in open('twitterkeys.txt')]
+
+CONSUMER_KEY = keys[0]
+CONSUMER_SECRET = keys[1]
+ACCESS_KEY = keys[2]
+ACCESS_SECRET = keys[3]
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+#Prepare the database
+
+corpus = 'poe'
+database = pickle.load(open(corpus+'.p'))
+
+#Generate a single sentence
+    
+def generate():
+    seed = database.keys()[random.randint(0, len(database.keys())-1)]
+    gentext = []
+    endpunct = ['.', '?', '!']
+    
+    while len(gentext) < 100:
+        gentext.append(seed[0])
+        new = database[seed][random.randint(0, len(database[seed])-1)]
+    
+        seed = (seed[1], new)
+
+    while True:
+        if gentext[1][0].isupper() and gentext[0][-1] in endpunct:
+            gentext.pop(0)
+            break
+        else:
+            gentext.pop(0)
+
+    sentence = []
+
+    for word in gentext:
+        if word[-1] not in endpunct:
+            sentence.append(word)
+        else:
+            sentence.append(word)
+            break
+        
+    text = ' '.join(sentence)
+    text = text.replace('\x97', '--') #replace em dashes with double hyphens
+
+    return text
+
+while True:
+    try:
+        tweet = generate()
+    except (IndexError, KeyError):
+        continue
+    
+    if  len(tweet) <= 140 and len(tweet) > 0 and tweet[-1] in ['.', '?', '!']:
+        api.update_status(tweet)
+        print tweet
+        break
